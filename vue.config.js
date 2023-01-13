51b3aa4cdc4efeaf3856df49cab1453ef2cab76e
FXJ <fanxj@syscloud.cn>
2023-01-13 20:20:02 +0800

    update config2

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,17 @@
 //通过判断是否为生产环境,设置不同的请求地址
-const isProduction = process.env.NODE_ENV === 'production'
-console.log('isProduction: ', isProduction);
-const target = isProduction ? 'http://www.coderfxj.site:3000' : 'http://127.0.0.1:3000'
+const isProduction = process.env.NODE_ENV === "production";
 module.exports = {
-    publicPath:'./',
-    devServer: {
-        proxy: {
-            '/api': {
-                target: target,
-                changeOrigin: true,
-                ws: true,
-                pathRewrite: {
-                    '^/api': ''
-                }
-            },
-        }
-    }
-}
+  publicPath: "./",
+  devServer: {
+    proxy: {
+      "/api": {
+        target: http://127.0.0.1:3000,
+        changeOrigin: true,
+        ws: true,
+        pathRewrite: {
+          "^/api": "",
+        },
+      },
+    },
+  },
+};
