508bb39a6db06a4ab7b56e78ad18c1ff6ebfd0a7
FXJ <fanxj@syscloud.cn>
2023-01-13 23:26:45 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 //通过判断是否为生产环境,设置不同的请求地址
 const isProduction = process.env.NODE_ENV === "production";
+console.log("%c [ isProduction ]-3", "font-size:14px; background:#4e7b39; color:#92bf7d;", isProduction);
 module.exports = {
   publicPath: isProduction ? "./" :'/',
   outputDir:process.env.OUTPUT_DIR,
