aad48c713d9878ba5998eae557dffb54cdd25ef6
root <tencent@gmail.com>
2023-01-13 23:00:29 +0800

    打包环境变量设置

--- src/router/index.js
+++ src/router/index.js
@@ -175,7 +175,7 @@ const routes = [
 
 const router = new VueRouter({
     mode: 'history',
-    base: process.env.BASE_URL,
+    base: '/music/',
     routes
 })
 const VueRouterPush = VueRouter.prototype.push
--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,8 @@
 const isProduction = process.env.NODE_ENV === "production";
 module.exports = {
   publicPath: "./",
+  outputDir:process.env.OUTPUT_DIR,
+  
   devServer: {
     proxy: {
       "/api": {
