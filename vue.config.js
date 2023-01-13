5c52875d3482c79247e3f7f7c575c55974495b4b
FXJ <fanxj@syscloud.cn>
2023-01-13 20:24:36 +0800

    update config3

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
   devServer: {
     proxy: {
       "/api": {
-        target: http://127.0.0.1:3000,
+        target: "http://127.0.0.1:3000",
         changeOrigin: true,
         ws: true,
         pathRewrite: {
